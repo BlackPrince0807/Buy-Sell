d437dd78fd528e799271b5d341345cde98b58351
Avazbek <foziljonovavazbek0807@gmail.com>
2022-01-31 10:29:43 +0500

    half commit

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ window.addEventListener('mousemove', function(event) {
 });
 
 ctx.fillStyle = 'white';
-ctx.font = '30px Verdana';
+ctx.font = '25px Verdana';
 ctx.fillText('A', 0, 30);
 const data = ctx.getImageData(0, 0, 100, 100);
 
@@ -28,25 +28,62 @@ class Particle {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.size = 3;
+        this.size = 1;
         this.baseX = this.x;
         this.baseY = this.y;
-        this.density = (Math.random() * 30) + 1;
+        this.density = (Math.random() * 30) + 5;
     }
     draw() {
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.ard(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
         ctx.fill();
+    }
+    update() {
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        let forceDirectionX = dx / distance;
+        let forceDirectionY = dy / distance;
+        let maxDistance = mouse.radius;
+        let force = (maxDistance - distance) / maxDistance;
+        let directionX = forceDirectionX * force * this.density;
+        let directionY = forceDirectionY * force * this.density;
+        if (distance < mouse.radius) {
+            this.x -= directionX;
+            this.y -= directionY;
+        } else {
+            if (this.x !== this.baseX) {
+                let dx = this.x - this.baseX;
+                this.x -= dx / 5;
+            }
+            if (this.y !== this.baseY) {
+                let dy = this.y - this.baseY;
+                this.y -= dy / 5;
+            }
+        }
     }
 }
 
 function init() {
     particleArray = [];
-    particleArray.push(new Particle(50, 50));
-    particleArray.push(new Particle(80, 50));
+    for (let i = 0; i < 800; i++) {
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        particleArray.push(new Particle(x, y));
+    }
 }
 
 init();
 console.log(particleArray);
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particleArray.length; i++) {
+        particleArray[i].draw();
+        particleArray[i].update();
+    }
+    requestAnimationFrame(animate);
+}
+animate();
